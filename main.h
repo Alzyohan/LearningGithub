0e452172cf18279bf67a54a6a9f588a90ba19a67
Yohan Boujon <yo.boujon@gmail.com>
2022-10-03 14:58:14 +0200

    Ajout de la fonction multiplyNumbers()

--- main.h
+++ main.h
@@ -1,8 +1,14 @@
 #include <stdio.h>
 
 int addNumbers(int a,int b);
+int multiplyNumbers(int a,int b);
 
 int addNumbers(int a,int b)
 {
     return a+b;
+}
+
+int multiplyNumbers(int a,int b)
+{
+    return a*b;
 }
