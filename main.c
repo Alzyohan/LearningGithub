eb99500965a260b4b94a80a99405aabe47e43ecf
Yohan Boujon <yo.boujon@gmail.com>
2022-10-03 14:27:27 +0200

    Ajout de la fonction addNumbers() dans le main.c

--- main.c
+++ main.c
@@ -1,6 +1,8 @@
-#include <stdio.h>
+#include "main.h"
 
 int main(void)
 {
     printf("Hello world!\n");
+    int i = addNumbers(12,23);
+    printf("Les nombres suivants ont été ajoutés : %d",i);
 }
